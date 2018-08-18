fa6ca62325d3d81d349fe37d1bdfb176bab1241d
ThisIsPIRI <thisispiri@gmail.com>
2018-08-18 16:01:50 +0900

    Add primitive updates

--- randomMath.js
+++ randomMath.js
@@ -1,2 +1,13 @@
+const pickRandom = function(fromHere) {
+	return fromHere[Math.floor(Math.random() * fromHere.length)]
+}
+
 const body = document.getElementsByTagName("body")[0];
-const constructs = [];
+const patterns = ["{%s}^{%s}", "{%s} + {%s}"];
+const update = function() {
+	const jax = pickRandom(MathJax.Hub.getAllJax(body));
+	MathJax.Hub.Queue(["Text", jax, sprintf(pickRandom(patterns), Math.random(), Math.random())]);
+	setTimeout(update, 1000);
+};
+//Wait until MathJax processes the initial jax. TODO: synchronize instead of relying on an arbitrary timeout
+setTimeout(update, 1000);
