11c71919e2dfe72948a6fd2f3b2e596f4726b1a6
ThisIsPIRI <thisispiri@gmail.com>
2018-08-19 21:00:34 +0900

    Separate UI-independent utilities

--- randomMath.js
+++ randomMath.js
@@ -1,105 +1,29 @@
-//TODO: Clean up
-const pickRandom = function(fromHere) {
-	return fromHere[Math.floor(Math.random() * fromHere.length)]
-};
-
-const chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-const genRandom = function(howMany) {
-	const result = Array(howMany);
-	for(var i = 0;i < howMany;i++) {
-		if(Math.random() > 0.5) { //A number
-			result[i] = (Math.random() * 100).toFixed(2);
-		}
-		else { //A symbol
-			result[i] = pickRandom(chars);
-		}
-	}
-	return result;
-};
-//TODO: Support codifying subtrees
-/**Codifies a Tree into plain LaTeX math.
- * @param node {Node} the root of the Tree.*/
-const codify = function(node) {
-	if(node.children.length === 0) { //Leaf node
-		return node.pat; //Return the number or symbol.
-	}
-	else if(node.depth === 0) {
-		var result = "";
-		for(var i = 0;i < node.children.length;i++) {
-			result = result.concat(codify(node.children[i]));
-			//if(i % 10 === 9) //TODO: Add an option to use this instead of the MathJax config for line break
-				//result += '\\\\';
-		}
-		return result; //Top-level return statement
-	}
-	else {
-		var result = [];
-		for(var i = 0;i < node.children.length;i++) {
-			result = result.concat(codify(node.children[i]));
-		}
-		return sprintf(node.pat.code, ...result);
-	}
-};
-
+//UI-specific code
 const toggle = function() {
 	stopped = !stopped;
 	if(!stopped) update();
-}
-
-const Pattern = function(code, vars) {
-	this.code = code;
-	this.vars = vars;
+	toggleButton.innerHTML = stopped ? "Start" : "Stop";
 };
-
-const mainDiv = document.getElementById("mainDiv");
-const topPatterns = [], middlePatterns = [];
-fileReader.read("TopPatterns.txt", function(read) {
-	tokens = fileReader.getTokensFrom(read, null, '\n');
-	for(var i = 1;i < tokens.length;i += 2) {
-		topPatterns.push(new Pattern(tokens[i - 1], parseInt(tokens[i])));
-	}
-});
-fileReader.read("MiddlePatterns.txt", function(read) {
-	tokens = fileReader.getTokensFrom(read, null, '\n');
-	for(var i = 1;i < tokens.length;i += 2) {
-		middlePatterns.push(new Pattern(tokens[i - 1], parseInt(tokens[i])));
-	}
-});
-const tree = new Tree();
-const DEPTH_LIMIT = 3;
-var stopped = false;
-
-tree.addNode(tree.root).pat = new Pattern("{%s} - {%s}", 2);
-tree.addNode(tree.allNodes[1][0]).pat = "1";
-tree.addNode(tree.allNodes[1][0]).pat = "i";
 
 const update = function() {
 	if(stopped)
 		return;
-	const jax = MathJax.Hub.getAllJax(mainDiv)[0];
-	const depth = Math.floor(Math.random() * (Math.min(tree.allNodes.length - 1, DEPTH_LIMIT)));
-	//if(tree.allNodes.length > 1 && Math.random() > 0.5) { //Remove a Node
-	if(false) {
-	}
-	else { //Add a Node
-		const parentNode = pickRandom(tree.allNodes[depth]);
-		var newNode;
-		if(parentNode.children.length === 0 || parentNode === tree.root)
-			newNode = tree.addNode(parentNode);
-		else
-			newNode = tree.replaceNode(pickRandom(parentNode.children));
-		if(parentNode === tree.root)
-			newNode.pat = pickRandom(topPatterns);
-		else
-			newNode.pat = pickRandom(middlePatterns);
-		genRandom(newNode.pat.vars).forEach(function(elem) {
-			tree.addNode(newNode).pat = elem;
-		});
-	}
-	MathJax.Hub.Queue(["Text", jax, codify(tree.root)]);
-	console.log(codify(tree.root));
-	console.log(tree);
+	modifyTree(tree, DEPTH_LIMIT, topPatterns, middlePatterns);
+	MathJax.Hub.Queue(["Text", MathJax.Hub.getAllJax(mainDiv)[0], codify(tree.root)]);
 	setTimeout(update, 500);
 };
-//Wait until MathJax processes the initial jax. TODO: Synchronize instead of relying on an arbitrary timeout
-setTimeout(update, 1000);
+const mainDiv = document.getElementById("mainDiv");
+const toggleButton = document.getElementById("toggleButton");
+const topPatterns = [], middlePatterns = [];
+const tree = new Tree();
+const DEPTH_LIMIT = 3;
+var stopped = false;
+
+//Initialization
+readPatterns("TopPatterns.txt", topPatterns);
+readPatterns("MiddlePatterns.txt", middlePatterns);
+tree.addNode(tree.root).pat = new Pattern("{%s} - {%s}", 2);
+tree.addNode(tree.allNodes[1][0]).pat = "1";
+tree.addNode(tree.allNodes[1][0]).pat = "i";
+//Wait until MathJax processes the initial jax and all Patterns are read. TODO: Synchronize instead of relying on an arbitrary timeout
+setTimeout(update, 2000);
---
+++ randomMathUtil.js
@@ -0,0 +1,85 @@
+//UI-independent utilities for generating random math
+const Pattern = function(code, vars) {
+	this.code = code;
+	this.vars = vars;
+};
+
+const readPatterns = function(fileName, toPushTo) {
+	fileReader.read(fileName, function(read) {
+		tokens = fileReader.getTokensFrom(read, null, '\n');
+		for(var i = 1;i < tokens.length;i += 2) {
+			toPushTo.push(new Pattern(tokens[i - 1], parseInt(tokens[i])));
+		}
+	});
+};
+
+const pickRandom = function(fromHere) {
+	return fromHere[Math.floor(Math.random() * fromHere.length)]
+};
+
+const genRandom = function(howMany) {
+	const result = Array(howMany);
+	for(var i = 0;i < howMany;i++) {
+		if(Math.random() > 0.5) { //A number
+			result[i] = (Math.random() * 100).toFixed(2);
+		}
+		else { //A symbol
+			result[i] = pickRandom(genRandom.chars);
+		}
+	}
+	return result;
+};
+//TODO: Move to somewhere else
+genRandom.chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+"\\alpha", "\\beta", "\\gamma", "\\delta", "\\epsilon", "\\zeta", "\\eta", "\\theta", "\\iota", "\\kappa", "\\lambda", "\\mu", "\\nu", "\\xi", "\\omicron",
+"\\pi", "\\rho", "\\sigma", "\\tau", "\\upsilon", "\\phi", "\\chi", "\\psi", "\\omega", "\\infty"];
+
+//TODO: Support codifying subtrees
+/**Codifies the Tree into a string.
+ * @param node {Node} the root of the Tree.*/
+const codify = function(node) {
+	if(node.children.length === 0) { //Leaf node
+		return node.pat; //Return the number or symbol.
+	}
+	else if(node.depth === 0) {
+		var result = "";
+		for(var i = 0;i < node.children.length;i++) {
+			result = result.concat(codify(node.children[i]));
+			//if(i % 10 === 9) //TODO: Add an option to use this instead of the MathJax config for line break
+				//result += '\\\\';
+		}
+		return result; //Top-level return statement
+	}
+	else {
+		var result = [];
+		for(var i = 0;i < node.children.length;i++) {
+			result = result.concat(codify(node.children[i]));
+		}
+		return sprintf(node.pat.code, ...result);
+	}
+};
+//TODO: Prevent empty parentheses
+const modifyTree = function(tree, depthLimit, topPatterns, middlePatterns) {
+	const depth = Math.floor(Math.random() * (Math.min(tree.allNodes.length - 1, depthLimit)));
+	//if(tree.allNodes.length > 1 && Math.random() > 0.8) { //Remove a Node
+	if(false) { //Remove something. TODO: implement
+	}
+	else { //Add something
+		const parentNode = pickRandom(tree.allNodes[depth]);
+		var newNode;
+		//Add or replace a Node
+		if(parentNode.children.length === 0 || parentNode === tree.root)
+			newNode = tree.addNode(parentNode);
+		else
+			newNode = tree.replaceNode(pickRandom(parentNode.children));
+		//Pick a Pattern for the Node
+		if(parentNode === tree.root)
+			newNode.pat = pickRandom(topPatterns);
+		else
+			newNode.pat = pickRandom(middlePatterns);
+		//Generate leaf Nodes for the Node
+		genRandom(newNode.pat.vars).forEach(function(elem) {
+			tree.addNode(newNode).pat = elem;
+		});
+	}
+};
