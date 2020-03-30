52bae0ce04c0facd3823cf4710373da36cf27eda
ThisIsPIRI <thisispiri@gmail.com>
2020-03-31 07:24:27 +0900

    함수 이름 우리말로 바꿈

--- Geummoi/moi.js
+++ Geummoi/moi.js
@@ -1,3 +1,12 @@
+Array.prototype.넣기 = Array.prototype.push;
+Array.prototype.하나하나 = Array.prototype.forEach;
+Array.prototype.타기 = Array.prototype.map;
+String.prototype.가르기 = String.prototype.split;
+console.쓰기 = console.log;
+ajaxRequester.가져오기 = ajaxRequester.request;
+ajaxRequester.가르기 = ajaxRequester.getTokensFrom;
+const 안고치손 = ajaxRequester; //안 고치고 가져오(AJAX)는 손
+
 const 낱말 = function(몇째, 말, 바꿈꼴, 붙임) {
 	this.몇째 = 몇째; //낱말이 말모이에서 몇 째인지
 	this.말 = 말; //낱말 글씨줄(문자열)
@@ -24,18 +33,18 @@ const 맨말읽기 = function(글) {
 };
 
 const 말읽기 = function(안글, 읽는수) {
-	안글 = 안글.split('/');
-	return 안글.map(읽는수);
+	안글 = 안글.가르기('/');
+	return 안글.타기(읽는수);
 }
 
 const 줄읽기 = function(줄) {
-	줄 = 줄.split(' ');
+	줄 = 줄.가르기(' ');
 	return new 다듬은말(말읽기(줄[0], 들온말읽기), 말읽기(줄[1], 맨말읽기));
 };
 
 const 말모이읽기 = function(안글) {
-	const 늘줄 = ajaxRequester.getTokensFrom(안글, null, '\n');
-	늘줄.forEach(function(줄) {
+	const 늘줄 = 안고치손.가르기(안글, null, '\n');
+	늘줄.하나하나(function(줄) {
 		if(줄[0] === '#')
 			return;
 		else if(줄[0] === '*')
@@ -43,11 +52,11 @@ const 말모이읽기 = function(안글) {
 		else if(줄 === '')
 			return; //TODO: parse sections?
 		else
-			늘낱말.push(줄읽기(줄));
+			늘낱말.넣기(줄읽기(줄));
 	});
-	console.log(늘낱말);
+	console.쓰기(늘낱말);
 };
 
 const 늘낱말 = [];
 
-ajaxRequester.request("manuri.malmoi", 말모이읽기);
+안고치손.가져오기("manuri.malmoi", 말모이읽기);
