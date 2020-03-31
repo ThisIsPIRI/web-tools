4b6377164b66e933d36b35092276a7b5c3117f20
ThisIsPIRI <thisispiri@gmail.com>
2020-04-01 00:25:22 +0900

    말모이찾기

--- Geummoi/moi.js
+++ Geummoi/moi.js
@@ -21,7 +21,7 @@ const 다듬은말 = function(몇째, 늘들온말, 늘맨말) {
 	this.몇째 = 몇째; //낱말이 말모이에서 몇 째인지 온셈이(정수)
 	this.늘들온말 = 늘들온말; //낱말 늘넣이
 	this.늘맨말 = 늘맨말; //낱말 늘넣이
-}
+};
 
 const 말읽기 = function(안글) {
 	안글 = 안글.가르기('/');
@@ -45,28 +45,49 @@ const 말읽기 = function(안글) {
 		만든것.말 = 글;
 		return 만든것;
 	});
-}
+};
 
 const 줄읽기 = function(줄) {
 	줄 = 줄.가르기(' ');
 	return new 다듬은말(0, 말읽기(줄[0]), 말읽기(줄[1]));
 };
 
-const 말모이읽기 = function(안글) {
-	const 늘줄 = 안고치손.가르기(안글, null, '\n');
-	늘줄.하나하나(function(줄) {
-		if(줄[0] === '#')
-			return;
-		else if(줄[0] === '*')
-			return; //TODO: footnote parsing
-		else if(줄 === '')
-			return; //TODO: parse sections?
-		else
-			늘낱말.넣기(줄읽기(줄));
+const 말모이읽기 = function(되부름) {
+	안고치손.가져오기("manuri.malmoi", function(안글) {
+		const 늘낱말 = [];
+		const 늘줄 = 안고치손.가르기(안글, null, '\n');
+		늘줄.하나하나(function(줄) {
+			if(줄[0] === '#')
+				return;
+			else if(줄[0] === '*')
+				return; //TODO: footnote parsing
+			else if(줄 === '')
+				return; //TODO: parse sections?
+			else
+				늘낱말.넣기(줄읽기(줄));
+		});
+		되부름(늘낱말);
 	});
-	console.쓰기(늘낱말);
 };
 
-const 늘낱말 = [];
-
-안고치손.가져오기("manuri.malmoi", 말모이읽기);
+const 말모이찾기 = function(말모이, 찾을말) {
+	const 만든것 = [];
+	말모이.하나하나(function(다듬하나) {
+		count++;
+		for(var ㅏ = 0;ㅏ < 다듬하나.늘들온말.length;ㅏ++) {
+			if(다듬하나.늘들온말[ㅏ].말.몇째칸(찾을말) != -1) {
+				만든것.넣기(다듬하나);
+				break;
+			}
+		}
+		if(만든것[만든것.length - 1] === 다듬하나)
+			return;
+		for(var ㅏ = 0;ㅏ < 다듬하나.늘맨말.length;ㅏ++) {
+			if(다듬하나.늘맨말[ㅏ].말.몇째칸(찾을말) != -1) {
+				만든것.넣기(다듬하나);
+				break;
+			}
+		}
+	});
+	return 만든것;
+};
---
+++ Geummoi/moinun.js
@@ -0,0 +1,7 @@
+var 말모이;
+const 가온 = document.getElementById("가온");
+
+말모이읽기(function(만든것) {
+	말모이 = 만든것;
+	console.쓰기(말모이찾기(말모이, "안녕"));
+});
