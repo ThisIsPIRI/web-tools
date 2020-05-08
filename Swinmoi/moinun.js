f2f12f3717c2ab44765efdcbfba95b9486b6f997
ThisIsPIRI <thisispiri@gmail.com>
2020-05-08 13:38:17 +0900

    늘말모이로 말모이서찾기

--- Swinmoi/moinun.js
+++ Swinmoi/moinun.js
@@ -6,7 +6,7 @@ EventTarget.prototype.귀더하기 = EventTarget.prototype.addEventListener;
 ajaxRequester.가져오기 = ajaxRequester.request;
 const 안고치손 = ajaxRequester; //안 고치고 가져오(AJAX)는 손
 
-var 흔한모이, 더한모이;
+var 흔한모이, 안흔모이;
 const 보여주는곳 = 누리쪽.바가름로찾기("보여주는곳");
 const 찾을말치 = 누리쪽.바가름로찾기("찾을말치");
 const 안흔찾기 = 누리쪽.바가름로찾기("안흔찾기");
@@ -51,9 +51,9 @@ const 찾아보여주기 = function(찾을말) {
 	var 찾을모이;
 	찾을말 = 찾을말.빈곳깎기();
 	if(안흔찾기.checked)
-		찾을모이 = 더한모이;
+		찾을모이 = [흔한모이, 안흔모이];
 	else
-		찾을모이 = 흔한모이;
+		찾을모이 = [흔한모이];
 	var 늘찾은말 = 말모이서찾기(찾을모이, 찾을말);
 	if(늘찾은말.length === 0) {
 		const 찾을말끝 = 찾을말.작은줄(찾을말.length - 2, 찾을말.length);
@@ -74,7 +74,7 @@ const 찾아보여주기 = function(찾을말) {
 안고치손.가져오기("manuri.swin", function(읽은모이) {
 	흔한모이 = 말모이아롬읽기(읽은모이);
 	안고치손.가져오기("anhen.swin", function(읽은안흔모이) {
-		더한모이 = 흔한모이.덧붙이기(말모이아롬읽기(읽은안흔모이));
+		안흔모이 = 말모이아롬읽기(읽은안흔모이);
 		보여주는곳.innerHTML = "";
 	});
 });
