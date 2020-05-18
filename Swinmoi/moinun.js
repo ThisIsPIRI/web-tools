103b4a50260c733384b511906967234cbb0f1f7d
ThisIsPIRI <thisispiri@gmail.com>
2020-05-18 15:31:51 +0900

    쉼으로 여러 낱말 함께찾기

--- Swinmoi/moinun.js
+++ Swinmoi/moinun.js
@@ -2,6 +2,8 @@
 
 document.바가름로찾기 = document.getElementById;
 const 누리쪽 = document;
+History.prototype.간곳넣기 = History.prototype.pushState;
+window.발자취 = window.history;
 const 누리봄 = window;
 EventTarget.prototype.귀더하기 = EventTarget.prototype.addEventListener;
 URLSearchParams.prototype.있나 = URLSearchParams.prototype.has;
@@ -33,15 +35,17 @@ const 다듬모습만들기 = function(보여줄말) {
 	보여줄말.늘맨말.하나하나(function(맨말) {
 		만든것 += 낱말모습만들기(맨말);
 	});
-	만든것 = 만든것.작은줄(0, 만든것.length - 2);
+	만든것 = 만든것.작은줄(0, 만든것.length - 2); //마지막 쉼을 뗌
 	if(보여줄말.붙임)
 		만든것 += "</span></p><p class=\"붙임\">" + 보여줄말.붙임.join("<br>") + "</p>";
+	else
+		만든것 += "</span></p>";
 	return 만든것;
 };
 
 /**
  @param 찾을말 {글씨줄} - 찾을 말*/
-const 찾아보여주기 = function(찾을말) {
+const 찾아모습만들기 = function(찾을말) {
 	var 만든것 = '';
 	var 찾을모이 = [흔한모이];
 	if(안흔찾기.checked)
@@ -55,30 +59,40 @@ const 찾아보여주기 = function(찾을말) {
 		if(찾을말끝 === "하다" || 찾을말끝 === "되다")
 			늘찾은말 = 말모이서찾기(찾을모이, 찾을말.작은줄(0, 찾을말.length - 2));
 		if(늘찾은말.length === 0)
-			만든것 = `'${찾을말}'은 우리 말모이에 없는 것 같습니다.<br>
+			만든것 = `<p>'${찾을말}'은 우리 말모이에 없는 것 같습니다.<br>
 			움직씨나 그림씨라면 -다 꼴로 찾아 보세요.<br>
-			'안 흔한 말' '엮은이가 맞춘말'을 켜 보세요.`;
+			'안 흔한 말' '엮은이가 맞춘말'을 켜 보세요.</p>`;
 	}
 	늘찾은말.하나하나(function(찾은말) {
 		만든것 += 다듬모습만들기(찾은말);
 	});
-	보여주는곳.innerHTML = 만든것;
+	return 만든것;
 };
 
 //단추나 줄바꿈 누를 때
 const 친말로찾기 = function() {
 	const 찾을말 = 찾을말치.value;
-	찾아보여주기(찾을말);
+	보여주는곳.innerHTML = 찾아모습만들기(찾을말);
 	if(찾은말적어놓기.checked)
-		누리봄.history.pushState(null, null, 누리봄.location.pathname + `?q=${찾을말}`);
+		누리봄.발자취.간곳넣기(null, null, 누리봄.location.pathname + `?q=${찾을말}`);
 };
 
 //새로 들어오거나 뒤로/앞으로 누를 때
 const 찾넘김로찾기 = function() {
 	const 찾넘김들 = new 찾넘김읽개(누리봄.location.search);
 	if(찾넘김들.있나('q')) {
-		찾을말치.value = 찾넘김들.얻기('q');
-		찾아보여주기(찾넘김들.얻기('q'));
+		var 찾을말 = 찾넘김들.얻기('q');
+		찾을말치.value = 찾을말;
+		//할것: 이걸 없애고 제대로 된 함께찾기를 만들 것
+		if(찾을말.몇째(',') != -1) {
+			찾을말 = 찾을말.가르기(',');
+			var 만든것 = "";
+			찾을말.하나하나(function(찾을말하나) {
+				만든것 += 찾아모습만들기(찾을말하나);
+			});
+			보여주는곳.innerHTML = 만든것;
+		}
+		else 보여주는곳.innerHTML = 찾아모습만들기(찾을말);
 	}
 }
 
